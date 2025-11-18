1f4f2a21ae0b977c605acd5fc094cf74145deb86
Anhelor <2238040405lex@gmail.com>
2025-11-18 16:21:40 +0800

    try to support speculative decoding

--- common/common.h
+++ common/common.h
@@ -414,7 +414,7 @@ struct common_params {
     bool verbose_prompt    = false; // print prompt tokens before generation
     bool display_prompt    = true;  // print prompt before generation
     bool no_kv_offload     = false; // disable KV offloading
-    bool warmup            = true;  // warmup run
+    bool warmup            = false;  // warmup run
     bool check_tensors     = false; // validate tensor data
     bool no_op_offload     = false; // globally disable offload host tensor operations to device
     bool no_extra_bufts    = false; // disable extra buffer types (used for weight repacking)
--- run_sparkinfer.sh
+++ run_sparkinfer.sh
@@ -2,21 +2,23 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
+draft_model="../Llama-160M-Chat-v1.gguf"
 model="../prosparse-llama-2-7b.gguf"
 model_split="../prosparse-llama-2-7b-sparkinfer-model-split.gguf"
 prompt="Bubble sort algorithm in python:"
 
 inference_opts=(
+    -md $draft_model
     -m $model
     -spif-ms $model_split
-    -cffn --no-mmap -ngl 999 -t 4
+    -cffn --no-mmap -ngld 999 -ngl 999 -t 4 -co
     --samplers "temperature;top_p" --temp 0.8 --top-p 0.95 -s 512
-    -p "$prompt" -n 128 -no-cnv
-    --no-warmup
+    --draft-min 3 --draft-max 5 --draft-p-min 0.5
+    -p "$prompt" -n 128
 )
 
-release_bin="./build_rel/bin/llama-cli"
-debug_bin="./build/bin/llama-cli"
+release_bin="./build_rel/bin/llama-speculative"
+debug_bin="./build/bin/llama-speculative"
 
 if [[ ${1-} == "release" ]]; then
     bin="$release_bin"
